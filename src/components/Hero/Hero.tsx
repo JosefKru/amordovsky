1a067dc5d827a8d34a642fd8c32a825013c76c27
Alyakin Ivan <i.alyakin@gk-adept.ru>
2025-02-14 21:08:12 +0300

    add adaptive version

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import './Hero.scss'
+import React from "react";
+import "./Hero.scss";
 
 const Hero: React.FC = () => {
   return (
-    <section className='hero'>
-      <h1 className='hero-title'>
-        Senior product designer helping company <br />
-        communicate with their audience in <br />
+    <section className="hero">
+      <h1 className="hero-title-1920">
+        Senior product designer helping company communicate with their audience
+        in a&nbsp;meaningful&nbsp;way
+      </h1>
+
+      <h1 className="hero-title-1000">
+        Senior&nbsp;product&nbsp;designer&nbsp;helping <br />
+        company&nbsp;communicate <br />
+        with&nbsp;their&nbsp;audience&nbsp;in <br />
         a&nbsp;meaningful&nbsp;way
       </h1>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
