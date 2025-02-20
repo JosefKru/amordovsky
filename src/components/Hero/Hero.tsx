de4042fb6948987b3d90ff70f2006d5d7b71f67b
Alyakin Ivan <i.alyakin@gk-adept.ru>
2025-02-20 20:08:35 +0300

    fix hero section

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -4,17 +4,19 @@ import "./Hero.scss";
 const Hero: React.FC = () => {
   return (
     <section className="hero">
-      <h1 className="hero-title-1920">
-        Senior product designer helping company communicate with their audience
-        in a&nbsp;meaningful&nbsp;way
-      </h1>
+      <div className="container">
+        <h1>
+          Senior product designer helping company communicate
+          with their audience in a&nbsp;meaningful&nbsp;way
+        </h1>
+      </div>
 
-      <h1 className="hero-title-1000">
-        Senior&nbsp;product&nbsp;designer&nbsp;helping <br />
-        company&nbsp;communicate <br />
-        with&nbsp;their&nbsp;audience&nbsp;in <br />
-        a&nbsp;meaningful&nbsp;way
-      </h1>
+      <div className="container-mobile">
+        <h1>
+          Senior&nbsp;product&nbsp;designer helping company communicate with
+          their audience in a&nbsp;meaningful&nbsp;way
+        </h1>
+      </div>
     </section>
   );
 };
