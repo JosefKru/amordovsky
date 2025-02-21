0152fe2d276f1a59ef471e05950f9d7858ebe477
JosefKru <josefkaru@gmail.com>
2025-02-21 19:25:45 +0300

    add svg icons and some fix

--- src/components/BurgerMenu/BurgerMenu.tsx
+++ src/components/BurgerMenu/BurgerMenu.tsx
@@ -1,12 +1,12 @@
 import { motion } from "framer-motion";
 import React, { useEffect, useState } from "react";
-import CloseIcon from "../../assets/icons/close-burger-menu.png";
-import OpenIcon from "../../assets/icons/open-burger-menu.png";
+import burgerIcon from "../../assets/icons/burgerIcon.svg";
+import closeIcon from "../../assets/icons/closeIcon.svg";
 import "./BurgerMenu.scss";
 
 const menuVariants = {
   open: { x: "0%", opacity: 1, transition: { duration: 0 } },
-  closed: { x: "-100%", transition: { duration: 0.1 } },
+  closed: { x: "-100%", opacity: 1, transition: { duration: 0 } },
 };
 
 const linkVariants = {
@@ -15,7 +15,6 @@ const linkVariants = {
     y: 0,
     transition: { delay: 0.1 * i, duration: 0.3 },
   }),
-  // closed: { y: 20 },
 };
 
 const BurgerMenu: React.FC = () => {
@@ -24,23 +23,24 @@ const BurgerMenu: React.FC = () => {
   const menuItems = ["Projects", "About", "Contact", "Rus"];
 
   useEffect(() => {
-    if (isOpen) {
-      document.body.classList.add("no-scroll");
-    } else {
-      document.body.classList.remove("no-scroll");
-    }
+    document.body.classList.toggle("no-scroll", isOpen);
   }, [isOpen]);
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
 
   return (
     <nav className="burger-menu">
       <button
         className="burger-icon"
-        onClick={() => setIsOpen((state) => !state)}
+        onClick={() => setIsOpen((prev) => !prev)}
+        aria-label="Toggle menu"
       >
         {isOpen ? (
-          <img src={CloseIcon} className="close-icon" />
+          <img src={closeIcon} className="closeMenu" alt="Open menu icon" />
         ) : (
-          <img src={OpenIcon} className="menu-icon" />
+          <img src={burgerIcon} className="openMenu" alt="Close menu icon" />
         )}
       </button>
 
@@ -55,7 +55,10 @@ const BurgerMenu: React.FC = () => {
             <motion.li key={item} custom={i} variants={linkVariants}>
               <a
                 href={`/${item.toLowerCase()}`}
-                onClick={() => setIsOpen(false)}
+                onClick={(e) => {
+                  e.preventDefault();
+                  closeMenu();
+                }}
               >
                 {item}
               </a>
