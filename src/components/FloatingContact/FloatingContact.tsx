0b817e22bde56aea9ae8e334bf72b95fbaab3220
vastolya <paramee@bk.ru>
2025-03-21 00:44:52 +0700

    add slow fade in animation on callback button

--- src/components/FloatingContact/FloatingContact.tsx
+++ src/components/FloatingContact/FloatingContact.tsx
@@ -42,75 +42,83 @@ const FloatingContact: React.FC = () => {
 
   return (
     <motion.div
-      className="floating-contact"
-      initial="circle"
-      animate={isOpen ? "expanded" : "circle"}
-      whileHover={isOpen ? "expanded" : "pill"}
-      variants={{
-        circle: {
-          width: 80,
-          height: 80,
-          borderRadius: 100,
-          transition: { type: "easeInOut", duration: 0.2 },
-          background: "white",
-        },
-        pill: {
-          width: 260,
-          height: 80,
-          borderRadius: 100,
-          transition: { type: "easeInOut", duration: 0.2 },
-          background: isClosing ? "white" : "black",
-          color: "white",
-        },
-        expanded: {
-          width: 260,
-          height: 316,
-          borderRadius: 32,
-          transition: { type: "easeInOut", duration: 0.3 },
-          boxShadow: "0px 2px 20px rgba(0, 0, 0, 0.1)",
-          background: "white",
-          color: "black",
-        },
-      }}
-      onClick={() => setIsOpen(true)}
-      exit={{ x: 100, y: -100, opacity: 0, transition: { duration: 0.3 } }}
-      onAnimationComplete={() => {
-        setTimeout(() => setIsClosing(false), 100);
-      }}
+      initial="hidden"
+      animate="visible"
+      exit="hidden"
+      variants={{ hidden: { opacity: 0 }, visible: { opacity: 1 } }}
+      transition={{ duration: 0.8, ease: "easeInOut" }}
     >
-      <div
-        className={`contact-details ${isOpen || isClosing ? "open " : ""}`}
-        ref={ref}
+      <motion.div
+        className="floating-contact"
+        initial="circle"
+        animate={isOpen ? "expanded" : "circle"}
+        whileHover={isOpen ? "expanded" : "pill"}
+        variants={{
+          circle: {
+            width: 80,
+            height: 80,
+            borderRadius: 100,
+            transition: { type: "easeInOut", duration: 0.2 },
+            background: "white",
+          },
+          pill: {
+            width: 260,
+            height: 80,
+            borderRadius: 100,
+            transition: { type: "easeInOut", duration: 0.2 },
+            background: isClosing ? "white" : "black",
+            color: "white",
+          },
+          expanded: {
+            width: 260,
+            height: 316,
+            borderRadius: 32,
+            transition: { type: "easeInOut", duration: 0.3 },
+            boxShadow: "0px 2px 20px rgba(0, 0, 0, 0.1)",
+            background: "white",
+            color: "black",
+          },
+        }}
+        onClick={() => setIsOpen(true)}
+        exit={{ x: 100, y: -100, opacity: 0, transition: { duration: 0.3 } }}
+        onAnimationComplete={() => {
+          setTimeout(() => setIsClosing(false), 100);
+        }}
       >
-        <div className="contact-header">
-          <img src={avatar} alt="Avatar" className="avatar-img" />
-          <h2>Contact&nbsp;me</h2>
-          {(isOpen || isClosing) && (
-            <img
-              src={closeIcon}
-              className="close-btn"
-              onClick={handleClose}
-              alt="Close menu icon"
-            />
-          )}
-        </div>
-
-        <div className="contact-info">
-          <div className="contact-item">
-            <p className="contact-type">Phone</p>
-            <p className="contact">+7 926 082 53 10</p>
+        <div
+          className={`contact-details ${isOpen || isClosing ? "open " : ""}`}
+          ref={ref}
+        >
+          <div className="contact-header">
+            <img src={avatar} alt="Avatar" className="avatar-img" />
+            <h2>Contact&nbsp;me</h2>
+            {(isOpen || isClosing) && (
+              <img
+                src={closeIcon}
+                className="close-btn"
+                onClick={handleClose}
+                alt="Close menu icon"
+              />
+            )}
           </div>
 
-          <div className="contact-item">
-            <p className="contact-type">E-mail</p>
-            <p className="contact">Andrei@gmail.com</p>
-          </div>
+          <div className="contact-info">
+            <div className="contact-item">
+              <p className="contact-type">Phone</p>
+              <p className="contact">+7 926 082 53 10</p>
+            </div>
 
-          <Link to="https://t.me/AndreyUX" className="telegram-btn">
-            Write to telegram
-          </Link>
+            <div className="contact-item">
+              <p className="contact-type">E-mail</p>
+              <p className="contact">Andrei@gmail.com</p>
+            </div>
+
+            <Link to="https://t.me/AndreyUX" className="telegram-btn">
+              Write to telegram
+            </Link>
+          </div>
         </div>
-      </div>
+      </motion.div>
     </motion.div>
   );
 };
--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -3,15 +3,11 @@ import { AnimatedWrapper } from "../AnimatedWrapper/AnimatedWrapper";
 import FloatingContact from "../FloatingContact/FloatingContact";
 import Header from "../Header/Header";
 
-export const pageVariants = {
-  hidden: { opacity: 0, y: -12 },
-  visible: { opacity: 1, y: 0 },
-};
-
 function MainLayout() {
   return (
     <>
       <Header />
+
       <FloatingContact />
 
       <AnimatedWrapper>
